c882c18861c85a060d9fc55116065b2089d3e0df
wuji <wuji@yuanduyun.com>
2025-11-19 11:18:38 +0800

    :tada: Initial commit.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import UnoCSS from 'unocss/vite'
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react(), cloudflare(), UnoCSS()],
 })
